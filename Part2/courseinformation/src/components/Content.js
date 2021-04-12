480701f0ea16dd51d56b3aa18cb2a5178cf68a61
“Rami <rami-alsebki@hotmail.com>
2021-04-12 04:50:11 +0300

    up till exercise 2.5

--- Part2/courseinformation/src/components/Content.js
+++ Part2/courseinformation/src/components/Content.js
@@ -1,16 +1,15 @@
 import React from "react";
 import Part from "./Part.js";
-import Sum from "./Sum.js";
 
 const Content = ({ courseParts }) => {
   return (
     <div>
-      <Part name={courseParts[0].name} exercises={courseParts[0].exercises} />
-      <Part name={courseParts[1].name} exercises={courseParts[1].exercises} />
-      <Part name={courseParts[2].name} exercises={courseParts[2].exercises} />
-      <Sum coursePartsExercises={courseParts} />
+      {courseParts.map(({ name, exercises, id }) => (
+      <Part key={id} partName={name} exercises={exercises} />
+    ))}
     </div>
   );
 };
 
 export default Content;
+
--- Part2/courseinformation/src/components/Course.js
+++ Part2/courseinformation/src/components/Course.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "./Header.js";
 import Content from "./Content.js";
+import Sum from "./Sum.js";
+
 
 const Course = ({ course }) => {
   return (
     <div>
       <Header header={course.name} />
       <Content courseParts={course.parts} />
+      <Sum coursePartsExercises={course.parts} />
+
     </div>
   );
 };
