ca245b623cf1d46cf1d2f5eb9815bedb3ab6e8b0
“Rami <rami-alsebki@hotmail.com>
2021-04-12 03:06:11 +0300

    Counting exercises function added

--- Part2/courseinformation/src/components/Content.js
+++ Part2/courseinformation/src/components/Content.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Part from './Part.js'
+import Sum from './Sum.js'
+
 
 const Content = ({courseParts}) => {
     return (
@@ -7,6 +9,7 @@ const Content = ({courseParts}) => {
         <Part name={courseParts[0].name} exercises={courseParts[0].exercises} />
         <Part name={courseParts[1].name} exercises={courseParts[1].exercises} />
         <Part name={courseParts[2].name} exercises={courseParts[2].exercises} />
+        <Sum coursePartsExercises={courseParts} />
       </div>
     )
   }
---
+++ Part2/courseinformation/src/components/Sum.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sum = ({coursePartsExercises}) => {
+    const totalSum = coursePartsExercises.reduce((acc, part) => acc + part.exercises, 0);
+  
+    return (
+      <p style={{fontWeight: "bold"}}>
+        Total of {totalSum} exercises
+      </p>
+    );
+  };
+
+export default Sum
